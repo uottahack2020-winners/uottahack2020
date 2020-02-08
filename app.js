7adcae8a64b37da16711319bac0700b908b9cf8d
gabrielgranata <ggran067@uottawa.ca>
2020-02-08 17:53:16 -0500

    can get lat long

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var firebase = require("firebase");
 var app = express();
 var geoip = require('geoip-lite');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+var constants = require('./utils/constants');
 
 //firebase config
 var firebaseConfig = {
@@ -13,7 +16,6 @@ var firebaseConfig = {
   messagingSenderId: "1098902235369",
   appId: "1:1098902235369:web:392eddfa9eac4d53996878"
 };
-
 
 
 init();
@@ -29,15 +31,35 @@ app.get('/', function(req, res, next) {
   res.send('Hello');
 });
 
-app.get('/latlong', function(req, res, next) {
+app.get('/latlong', async function(req, res, next) {
   //get ip address, feed into a geolookup
-  var ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  let geo = geoip.lookup(ip);
-  res.json({latLong: geo.ll});
+  // var ip = req.headers['x-forwarded-for'] || 
+  //    req.connection.remoteAddress || 
+  //    req.socket.remoteAddress ||
+  //    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  // let geo = geoip.lookup(ip);
+
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${constants.uOttawaAddress}&key=${constants.apiKey}`;
+  var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url );
+    xmlHttp.send( null );
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4) {
+        let resJson = JSON.parse(xmlHttp.responseText);
+
+        let latLong = resJson.results[0].geometry.location;
+        // let lat = resJson.results[0].geometry.location.lat;
+        // let long = resJson.responseText.results[0].geometry.location.long;
+
+        res.send(latLong);
+      } 
+    }
+
+  // let lat = latLong.results.geometry.location.lat;
+  // let long = latLong.results.geometry.location.lng;
+  // res.json({lat: lat, long: long});
 });
+
 
 
 app.post('/users/drivers', function(req, res, next){
@@ -105,5 +127,6 @@ app.post('/users/business', function(req,res, next) {
 //       });
 
 // error handler
-
-module.exports = app;
+app.listen(3000, function() {
+  console.log('server running');
+})
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const uOttawaAddress = '75+Laurier+Ave+E,+Ottawa,+ON+K1N+6N5';
+
+const apiKey = 'AIzaSyCCa30P9-jhu-MNAF8GlZ1hP5nSF1Lz_jo'
+
+module.exports = {uOttawaAddress, apiKey}
