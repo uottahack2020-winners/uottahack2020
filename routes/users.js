9c45bac8710550e8749dc8f5ea961ce392bb2b8b
gabrielgranata <ggran067@uottawa.ca>
2020-02-08 11:20:03 -0500

    'maybe'

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.post('/users/drivers?', function(req, res, next){
+router.post('/drivers', function(req, res, next){
 
   var db = firebase.firestore();
   //use id to post
