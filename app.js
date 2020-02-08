47f8f0667096f231b22f57d9352d81ba35eba57d
gabrielgranata <ggran067@uottawa.ca>
2020-02-08 12:01:43 -0500

    starting online

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var firebase = require("firebase");
 var app = express();
 
@@ -17,27 +13,19 @@ var firebaseConfig = {
   appId: "1:1098902235369:web:392eddfa9eac4d53996878"
 };
 
+
+init();
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+function init() {
+  firebase.initializeApp(firebaseConfig);
+  let db = firebase.database();
+};
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 app.get('/', function(req, res, next) {
-
-});
+  res.send('Hello');
+})
 
 app.post('/users/drivers', async function(req, res, next){
 
@@ -74,18 +62,7 @@ app.post('/users/drivers', async function(req, res, next){
 //       });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(3000, function(){
 
-app.set('port', 3000);
-
-app.listen(3000, function() {
-  
 });
