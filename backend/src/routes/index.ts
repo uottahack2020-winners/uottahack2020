ccf9f547890abb2c2a5b31af457eb1fb4ee39840
David Meribe <meribe.david@yahoo.com>
2020-02-08 01:49:52 -0500

    starting from scratch, with firebase

--- backend/src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-const routes = [
-    
-];
-
-export { routes };
--- backend/src/services/logService.ts
+++
@@ -1,9 +0,0 @@
-import * as bunyan from 'bunyan';
-
-const logger = bunyan.createLogger({
-  name: process.env.npm_package_name || 'innova-server',
-  level: (process.env.LOG_LEVEL as bunyan.LogLevelString) || 'debug',
-  serializers: bunyan.stdSerializers
-});
-
-export { logger };
