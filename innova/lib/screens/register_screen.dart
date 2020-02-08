94eaf0b9643c93cb867df5c81231d09427e98dc2
gabrielgranata <ggran067@uottawa.ca>
2020-02-08 05:12:11 -0500

    some more inputs

--- innova/lib/screens/register_screen.dart
+++ innova/lib/screens/register_screen.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:innova/constants/input_decoration.dart';
 
+enum UserType {
+  driver,
+  business
+}
+
 class RegisterScreen extends StatefulWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
@@ -14,6 +19,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   String password;
   String firstName;
   String lastName;
+  UserType userType = UserType.business;
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -26,61 +32,104 @@ class _RegisterScreenState extends State<RegisterScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 10),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: inputDecoration,
+                        onChanged: (value) {
+                          firstName = value;
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: inputDecoration,
+                        onChanged: (value) {
+                          lastName = value;
+                        },
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(15, 0, 15, 10),
+                child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    decoration: inputDecoration,
+                    onChanged: (value) {
+                      email = value;
+                    }),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  obscureText: false,
+                  textAlign: TextAlign.center,
+                  decoration: inputDecoration,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+              ),
               Row(
                 children: <Widget>[
                   Expanded(
-                    child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        textAlign: TextAlign.center,
-                        decoration: inputDecoration,
+                    child: ListTile(
+                        leading: Radio(
+                          value: UserType.business,
+                          groupValue: userType,
+                          onChanged: (UserType value) {
+                            setState(() {
+                              userType = value;
+                            });
+                          },
+                        ),
+                      title: Text(
+                        'business'
+                      ),
                     ),
                   ),
                   Expanded(
-                    child: TextField(
-                        textAlign: TextAlign.center,
-                        decoration: inputDecoration,
+                    child: ListTile(
+                        leading: Radio(
+                          value: UserType.driver,
+                          groupValue: userType,
+                          onChanged: (UserType value) {
+                            setState(() {
+                              userType = value;
+                            });
+                          },
+                        ),
+                      title: Text(
+                        'driver'
+                      ),
                     ),
-                  ),
-                ]
+                  )
+                ],
               ),
-//            TextField(
-//                keyboardType: TextInputType.emailAddress,
-//                textAlign: TextAlign.center,
-//                decoration: InputDecoration(
-//                    hintText: 'Enter a value',
-//                    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-//                    border: OutlineInputBorder(
-//                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-//                    ),
-//                    enabledBorder: OutlineInputBorder(
-//                      borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-//                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-//                    ),
-//                    focusedBorder: OutlineInputBorder(
-//                      borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-//                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-//                    ),
-//                ),
-//                onChanged: (value) {
-//                  firstName = value;
-//                }
-//            ),
-//              TextField(
-//                obscureText: false,
-//                textAlign: TextAlign.center,
-//                onChanged: (value) {
-//                  password = value;
-//                },
-//              ),
-                Padding(
+              Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0),
                 child: Material(
-                  elevation: 5.0, color: Colors.blue,
+                  elevation: 5.0,
+                  color: Colors.blue,
                   borderRadius: BorderRadius.circular(30.0),
                   child: MaterialButton(
                     onPressed: () async {
                       try {
-                        final firebaseUser = await _auth.createUserWithEmailAndPassword(email: email.trim(), password: password);
+                        final firebaseUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email.trim(), password: password);
 
                         if (firebaseUser != null) {
                           Navigator.pushNamed(context, 'homeScreen');
@@ -88,7 +137,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       } catch (e) {
                         print(e);
                       }
-
                     },
                     minWidth: 200.0,
                     height: 42.0,
