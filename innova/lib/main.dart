b872cdedf51e25c249d5ce7274db68c882083d41
gabrielgranata <ggran067@uottawa.ca>
2020-02-08 03:04:28 -0500

    register functionality

--- innova/lib/main.dart
+++ innova/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:innova/screens/home-screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +14,7 @@ class MyApp extends StatelessWidget {
       initialRoute: 'welcomeScreen',
       routes: {
         'welcomeScreen': (context) => WelcomeScreen(),
+        'homeScreen': (context) => HomeScreen(),
       }
     );
   }
@@ -26,6 +29,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   String email;
   String password;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -38,36 +42,43 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 textAlign: TextAlign.center,
                 onChanged: (value) {
                   email = value;
-                  print(email);
                 }
             ),
             TextField(
-              obscureText: true,
+              obscureText: false,
               textAlign: TextAlign.center,
               onChanged: (value) {
                 password = value;
-                print(password);
               },
             ),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-              elevation: 5.0, color: Colors.blue,
-              borderRadius: BorderRadius.circular(30.0),
-              child: MaterialButton(
-                onPressed: () {
-                  
-                },
-                minWidth: 200.0,
-                height: 42.0,
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
+                child: Material(
+                elevation: 5.0, color: Colors.blue,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () async {
+                    try {
+                      final firebaseUser = await _auth.createUserWithEmailAndPassword(email: email.trim(), password: password);
+
+                      if (firebaseUser != null) {
+                        Navigator.pushNamed(context, 'homeScreen');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
           ),
           ],
         ),
